5ede0e226016b831f505f4b03dc5cdcba419fb53
mykemigga <mig.a.garc@gmail.com>
2020-02-19 18:11:13 -0600

    Create Chap 4 bio.rkt

---
+++ Chap 4 bio.rkt
@@ -0,0 +1,21 @@
+#lang mc-coach/bio
+
+(bio
+  #:first-name  “Michael"
+  #:profile-pic (square 40 ‘solid 'red)
+  #:coach-story “Once you get people laughing, they are listening and you can tell them almost anything.
+When I was younger,
+growing up in a household with two languages made for an interesting perspective of the way I spoke to others.
+Respect followed, of course as my parented demanded I rely not solely on brute strength,
+that my mind could move mountains where my body would become exhausted.
+I owe a lot of my mannerisms from my father,
+who spoke only when he deemed it necessary, and not waste a word so callously.
+My mother taught me spanish in an effort to speak and understand my other family members who hail from Mexico and other spanish dominated lands.
+Learning to code and all the practices that come with it are a gentle reminder of the language format I used before to develop my english and spanish speaking skills.
+To use words where there were only gaps of information instead.
+Gestures, when body language came in to play.
+Context, where there are pieces to a story that haven't had a chance to show themselves
+just yet.
+Public speaking is not simply speaking in public, it is having a voice and a message to deliver to the ears and hearts of those with capacity to listen and understand.
+Now, with the addition of the internet,
+the entire world is a stage and I am under it's spotlight.")
